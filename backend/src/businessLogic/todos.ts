fe9d39ce50a1315595b966e491fff3c383ca3330
Biruk Ephrem <birukeph@gmail.com>
2022-11-30 19:52:41 +0300

    implemented create todos

---
+++ backend/src/businessLogic/todos.ts
@@ -0,0 +1,34 @@
+import { TodoAccess } from '../dataLayer/todosAcess'
+import { AttachmentUtils } from '../helpers/attachmentUtils';
+import { TodoItem } from '../models/TodoItem'
+import { CreateTodoRequest } from '../requests/CreateTodoRequest'
+// import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+import { createLogger } from '../utils/logger'
+import * as uuid from 'uuid'
+// import * as createError from 'http-errors'
+
+// TODO: Implement businessLogic
+const logger = createLogger('TodoAccess')
+const attachmentUtils = new AttachmentUtils()
+const todoAccess = new TodoAccess()
+
+export async function createTodo(
+	newTodo: CreateTodoRequest,
+	userId: string
+): Promise<TodoItem> {
+	logger.info('create todo called')
+	const todoId = uuid.v4()
+	const createdAt = new Date().toISOString()
+	const s3AttachmentUrl = attachmentUtils.getAttachmentUrl(todoId)
+
+	const newItem = {
+		userId,
+		todoId,
+		createdAt,
+		done: false,
+		attachmentUrl: s3AttachmentUrl,
+		...newTodo
+	}
+
+	return await todoAccess.createTodoItem(newItem)
+}
--- client/src/config.ts
+++ client/src/config.ts
@@ -5,7 +5,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'burka.us.auth0.com',            // Auth0 domain
+  clientId: '8b0UatsLsD4UFEMWRFGrqhnvKpgMaXQr',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
